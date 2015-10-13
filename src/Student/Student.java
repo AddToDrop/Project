217832f458b0184b7c63aea10d2459d312c353f7
Alex <dohyungjeong@gmail.com>
2015-10-13 23:32:19 +0800

    Update of Student Class(not complete)

--- src/Student/Student.java
+++ src/Student/Student.java
@@ -1,4 +1,46 @@
 package Student;
 
+import java.util.ArrayList;
+
+import Course.Course;
+
 public class Student {
+	//JEONG Alex
+	String name;
+	String EID;
+	String password;
+	String programme;
+	ArrayList<Course> prevTaken;
+	ArrayList<Course> registered;
+	
+	public Student(String studentInfo){
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public String getEID() {
+		return this.EID;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public String getProgramme() {
+		return this.programme;
+	}
+
+	public ArrayList<Course> getPrevTaken() {
+		return this.prevTaken;
+	}
+	public void setRegistered(ArrayList<Course> registered) {
+		this.registered=registered;
+	}
+	public ArrayList<Course> getRegistered() {
+		return this.registered;
+	}
+	
 }
