5fe5fc18332c25d688cc86f5ad06af4cb8c82177
lyanlokecho <lyanlokecho@gmail.com>
2015-10-23 21:20:30 +0800

    update course function

--- src/Course/Course.java
+++ src/Course/Course.java
@@ -1,16 +1,29 @@
 package Course;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
 import Session.Session;
+import Setup.Admin;
 
 public abstract class Course {
 	private String courseCode;
 	private String courseTitle;
-	//private HashMap key: CRN(string) value:session(object);
+	private HashMap<String, Session> sessionList;
 	
 	public Course(String[] courseInfo){ //add hashmap and store
 		courseCode = courseInfo[1];
 		courseTitle = courseInfo[2];
+		HashMap<String, Session> sessionList = new HashMap<String, Session>();
+		
+		for(int i=3; i<courseInfo.length; i++){
+			String CRN = courseInfo[i];
+			Session session = Admin.searchSession(CRN);
+			if(session != null){
+				sessionList.put("CRN", session);
+			}
+		}
 		
 	}
 	
@@ -19,8 +32,41 @@ public abstract class Course {
 	}
 
 	//function arraylist getCRN ()
+	public ArrayList getSessionCRN(){
+		ArrayList<String> sessionCRNList = new ArrayList<String>();
+		Set<String> keys = sessionList.keySet();
+		
+		for(String key:keys) {
+			String code = sessionList.get(key).getCRN();
+			sessionCRNList.add(code);
+		}
+		return sessionCRNList;
+	}
 
-	//function getinfo(crn, string time/day)
+	//function getinfo(crn, string time/day) non static
+	public int getInfo(String CRN, String info){
+		Session session = sessionList.get(CRN);
+		int time = 0;
+		if(info.equals("day"))
+			time = session.getDay();
+		else if(info.equals("start"))
+			time = session.getStart();
+		else if(info.equals("end"))
+			time = session.getEnd();
+		return time;
+	}
 	
+	//static version
+	public static int getSessionInfo(String CRN, String info){
+		Session session = Admin.searchSession(CRN);
+		int time = 0;
+		if(info.equals("day"))
+			time = session.getDay();
+		else if(info.equals("start"))
+			time = session.getStart();
+		else if(info.equals("end"))
+			time = session.getEnd();
+		return time;
+	}
 
 }
