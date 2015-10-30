c202c6be1716ea9215a2d2e1049bee6b2c088144
lyanlokecho <lyanlokecho@gmail.com>
2015-10-30 14:10:17 +0800

    Update Validator.java
    Suggest Time Conflict Validation flow

--- src/Utilities/Validator.java
+++ src/Utilities/Validator.java
@@ -9,6 +9,8 @@ public class Validator {
 		1st: new start in between original start and end
 		2nd: new end in between original start and end
 		3rd: original start in between new start and end
+		detail please go to=>
+		https://docs.google.com/drawings/d/1TIIiGRoavAuEsFviz_qOEFtDer6gyT-bWEETRtXjP_I/edit
 	*/
 	
 	//Check studied Validation
