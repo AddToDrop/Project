91fb54d181004ff9855e1e2618dd9240a97405d6
lyanlokecho <lyanlokecho@gmail.com>
2015-10-30 13:56:54 +0800

    Update Validator.java
    Suggested time validator flow

--- src/Utilities/Validator.java
+++ src/Utilities/Validator.java
@@ -5,6 +5,11 @@ public class Validator {
 	//Password Validation
 	
 	//Time Conflict Validation
+	/*
+		1st: new start in between original start and end
+		2nd: new end in between original start and end
+		3rd: original start in between new start and end
+	*/
 	
 	//Check studied Validation
 	
