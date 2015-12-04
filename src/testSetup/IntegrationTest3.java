faf5aaf182820da4387e941a3661cd61bf77d30e
yanloklai3 <yanloklai3@XNB40.cs.cityu.edu.hk>
2015-12-04 16:23:11 +0800

    IntegratedTest3

--- src/testSetup/IntegrationTest3.java
+++ src/testSetup/IntegrationTest3.java
@@ -7,6 +7,7 @@ package testSetup;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import org.junit.Test;
 
 import college.College;
@@ -46,6 +47,27 @@ public class IntegrationTest3 extends TestCase{
 			finalResult = true;
 		}
 		assertEquals(finalResult, true);
+    }
+	
+	@Test
+    public void test_getName_01() throws Exception {
+		College c1 = new Law("Law LLB");
+		String result = c1.getName();
+		assertEquals(result, "Law");
+    }
+	
+	@Test
+    public void test_getName_02() throws Exception {
+		College c1 = new SCM("SCM BACM/BSCCM/BAS");
+		String result = c1.getName();
+		assertEquals(result, "SCM");
+    }
+	
+	@Test
+    public void test_getName_03() throws Exception {
+		College c1 = new SEE("SEE BEngSE");
+		String result = c1.getName();
+		assertEquals(result, "SEE");
     }
 	
 }
