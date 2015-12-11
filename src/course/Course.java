3c22fd0a03f13fbadda5656135d7759bdcf199cf
echo <lyanlokecho@gmail.com>
2015-12-11 15:27:18 +0800

    Remove three functions that not in use

--- src/course/Course.java
+++ src/course/Course.java
@@ -53,44 +53,6 @@ public abstract class Course {
 		return session;
 	}
 	
-	//function arraylist getCRN ()
-	public ArrayList<String> getSessionCRN(){
-		ArrayList<String> sessionCRNList = new ArrayList<String>();
-		Set<String> keys = sessionList.keySet();
-		
-		for(String key:keys) {
-			String code = sessionList.get(key).getCRN();
-			sessionCRNList.add(code);
-		}
-		return sessionCRNList;
-	}
-
-	//function getinfo(crn, string time/day) non static
-	public int getInfo(String CRN, String info){
-		Session session = sessionList.get(CRN);
-		int time = 0;
-		if(info.equals("day"))
-			time = session.getDay();
-		else if(info.equals("start"))
-			time = session.getStart();
-		else if(info.equals("end"))
-			time = session.getEnd();
-		return time;
-	}
-	
-	//static version
-	public static int getSessionInfo(String CRN, String info){
-		Admin admin = Admin.getInstance();
-		Session session = admin.getSession(CRN);
-		int time = 0;
-		if(info.equals("day"))
-			time = session.getDay();
-		else if(info.equals("start"))
-			time = session.getStart();
-		else if(info.equals("end"))
-			time = session.getEnd();
-		return time;
-	}
 
 	public boolean isEquals(Course course){
 		if(this.getCourseCode().equals(course.getCourseCode()))
